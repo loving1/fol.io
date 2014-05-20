26a4e2dce590f31dfc2d8c2dad951c5cd7d74c44
Rose <roseannhaft@gmail.com>
2014-05-19 17:55:48 -0700

    ux prof-desk

---
+++ php/index.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * Run PHP Code
+ * 
+ * This script gives you the ability to quickly test snippets of PHP code locally.
+ *
+ * @copyright  Copyright 2011-2012, Website Duck LLC (http://www.websiteduck.com)
+ * @link       http://github.com/websiteduck/Run-PHP-Code Run PHP Code
+ * @license    MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+ 
+//This application is meant to be run locally and should not be made publicly accessible.
+if (!in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) die();
+
+define('NL', PHP_EOL);
+
+if (isset($_POST['phprun_action']) && $_POST['phprun_action'] == 'download') {
+	if (substr($_POST['phprun_filename'], -4) !== '.php') $_POST['phprun_filename'] .= '.php';
+	header('Content-Type: text/plain');
+	header('Content-Disposition: attachment; filename=' . $_POST['phprun_filename']);
+	echo $_POST['phprun_code'];
+	die();
+}
+
+if (isset($_POST['phprun_action']) && $_POST['phprun_action'] == 'run') {
+	header('Expires: Mon, 16 Apr 2012 05:00:00 GMT');
+	header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); 
+	header('Cache-Control: no-store, no-cache, must-revalidate'); 
+	header('Cache-Control: post-check=0, pre-check=0', false);
+	header('Pragma: no-cache');
+	header('X-XSS-Protection: 0');
+	ini_set('display_errors', 1);
+	switch ($_POST['error_reporting'])
+	{
+		case 'fatal': error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR); break;
+		case 'warning': error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR | E_WARNING); break;
+		case 'deprecated': error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR | E_WARNING | E_DEPRECATED | E_USER_DEPRECATED); break;
+		case 'notice': error_reporting(E_ERROR | E_PARSE | E_COMPILE_ERROR | E_WARNING | E_DEPRECATED | E_USER_DEPRECATED | E_NOTICE); break;
+		case 'all': error_reporting(-1); break;
+		case 'none': default: error_reporting(0); break;
+	}
+	$phprun_code = '?>' . ltrim($_POST['phprun_code']);
+	ob_start();
+	eval($phprun_code);
+	$phprun_html = ob_get_clean();
+	if (isset($_POST['pre_wrap'])) $phprun_html = '<pre>' . $phprun_html . '</pre>';
+	if (isset($_POST['colorize'])) $phprun_html = '<link rel="stylesheet" href="css/colorize.css">' . $phprun_html;
+	echo $phprun_html;
+	die();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Run PHP Code</title>
+		<script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+		<script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>
+		<script type="text/javascript" src="js/ace/ace.js" charset="utf-8"></script>
+		<script type="text/javascript" src="js/run_php_code.js"></script>
+
+		<link rel="shortcut icon" href="favicon.ico" >
+		<link rel="stylesheet" href="css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/run_php_code.css">
+	</head>
+	<body>		
+		<form id="run_php_form" method="POST" action="" target="result_frame" onsubmit="run_php_form_submit()">
+			<input type="hidden" name="phprun_action" value="run" />
+			<input type="hidden" name="phprun_filename" value="" />
+			<div id="title_bar">
+				<div id="title">Run PHP Code</div>
+				
+				<div class="drop"><span>File</span>
+					<div>
+						<button class="button" id="btn_import" type="button">Remote Import...</button>
+						<button class="button" id="btn_download" type="button">Download...</button>
+					</div>
+				</div
+				><div class="drop"><span>Options</span>
+					<div>
+						<input type="checkbox" id="mnu_colorize" name="colorize" /><label for="mnu_colorize"><span></span> Colorize</label>
+						<input type="checkbox" id="mnu_external_window" /><label for="mnu_external_window"><span></span> External Window</label>
+						<input type="checkbox" id="mnu_pre_wrap" name="pre_wrap" /><label for="mnu_pre_wrap"><span></span> &lt;pre&gt; Wrap</label>
+						<div class="subdrop">
+							Error Reporting
+							<div>
+								<input type="radio" id="mnu_er_none" name="error_reporting" value="none" /><label for="mnu_er_none"><span></span> None</label>
+								<input type="radio" id="mnu_er_fatal" name="error_reporting" value="fatal" /><label for="mnu_er_fatal"><span></span> Fatal</label>
+								<input type="radio" id="mnu_er_warning" name="error_reporting" value="warning" /><label for="mnu_er_warning"><span></span> Warning</label>
+								<input type="radio" id="mnu_er_deprecated" name="error_reporting" value="deprecated" /><label for="mnu_er_deprecated"><span></span> Deprecated</label>
+								<input type="radio" id="mnu_er_notice" name="error_reporting" value="notice" /><label for="mnu_er_notice"><span></span> Notice</label>
+								<input type="radio" id="mnu_er_all" name="error_reporting" value="all" /><label for="mnu_er_all"><span></span> All</label>
+							</div>
+						</div>
+					</div>
+				</div
+				><div class="drop"><span>Themes</span>
+					<div>
+						<div class="subdrop">
+							Light
+							<div>
+								<input type="radio" id="mnu_theme_chrome" name="theme" value="chrome" /><label for="mnu_theme_chrome"><span></span> Chrome</label>
+								<input type="radio" id="mnu_theme_clouds" name="theme" value="clouds" /><label for="mnu_theme_clouds"><span></span> Clouds</label>
+								<input type="radio" id="mnu_theme_crimson_editor" name="theme" value="crimson_editor" /><label for="mnu_theme_crimson_editor"><span></span> Crimson Editor</label>
+								<input type="radio" id="mnu_theme_dawn" name="theme" value="dawn" /><label for="mnu_theme_dawn"><span></span> Dawn</label>
+								<input type="radio" id="mnu_theme_dreamweaver" name="theme" value="dreamweaver" /><label for="mnu_theme_dreamweaver"><span></span> Dreamweaver</label>
+								<input type="radio" id="mnu_theme_eclipse" name="theme" value="eclipse" /><label for="mnu_theme_eclipse"><span></span> Eclipse</label>
+								<input type="radio" id="mnu_theme_github" name="theme" value="github" /><label for="mnu_theme_github"><span></span> GitHub</label>
+								<input type="radio" id="mnu_theme_katzenmilch" name="theme" value="katzenmilch" /><label for="mnu_theme_katzenmilch"><span></span> Katzenmilch</label>
+								<input type="radio" id="mnu_theme_kuroir" name="theme" value="kuroir" /><label for="mnu_theme_kuroir"><span></span> Kuroir</label>
+								<input type="radio" id="mnu_theme_solarized_light" name="theme" value="solarized_light" /><label for="mnu_theme_solarized_light"><span></span> Solarized Light</label>
+								<input type="radio" id="mnu_theme_textmate" name="theme" value="textmate" /><label for="mnu_theme_textmate"><span></span> TextMate</label>
+								<input type="radio" id="mnu_theme_tomorrow" name="theme" value="tomorrow" /><label for="mnu_theme_tomorrow"><span></span> Tomorrow</label>
+								<input type="radio" id="mnu_theme_xcode" name="theme" value="xcode" /><label for="mnu_theme_xcode"><span></span> XCode</label>
+							</div>
+						</div>
+						<div class="subdrop">
+							Dark
+							<div>
+								<input type="radio" id="mnu_theme_ambiance" name="theme" value="ambiance" /><label for="mnu_theme_ambiance"><span></span> Ambiance</label>
+								<input type="radio" id="mnu_theme_chaos" name="theme" value="chaos" /><label for="mnu_theme_chaos"><span></span> Chaos</label>
+								<input type="radio" id="mnu_theme_clouds_midnight" name="theme" value="clouds_midnight" /><label for="mnu_theme_clouds_midnight"><span></span> Clouds Midnight</label>
+								<input type="radio" id="mnu_theme_cobalt" name="theme" value="cobalt" /><label for="mnu_theme_cobalt"><span></span> Cobalt</label>
+								<input type="radio" id="mnu_theme_idle_fingers" name="theme" value="idle_fingers" /><label for="mnu_theme_idle_fingers"><span></span> Idle Fingers</label>
+								<!-- <input type="radio" id="mnu_theme_kr" name="theme" value="kr" /><label for="mnu_theme_kr"><span></span> krTheme</label> -->
+								<input type="radio" id="mnu_theme_merbivore" name="theme" value="merbivore" /><label for="mnu_theme_merbivore"><span></span> Merbivore</label>
+								<input type="radio" id="mnu_theme_merbivore_soft" name="theme" value="merbivore_soft" /><label for="mnu_theme_merbivore_soft"><span></span> Merbivore Soft</label>
+								<input type="radio" id="mnu_theme_monokai" name="theme" value="monokai" /><label for="mnu_theme_monokai"><span></span> Monokai</label>
+								<input type="radio" id="mnu_theme_mono_industrial" name="theme" value="mono_industrial" /><label for="mnu_theme_mono_industrial"><span></span> Mono Industrial</label>
+								<input type="radio" id="mnu_theme_pastel_on_dark" name="theme" value="pastel_on_dark" /><label for="mnu_theme_pastel_on_dark"><span></span> Pastel on dark</label>
+								<input type="radio" id="mnu_theme_solarized_dark" name="theme" value="solarized_dark" /><label for="mnu_theme_solarized_dark"><span></span> Solarized Dark</label>
+								<input type="radio" id="mnu_theme_terminal" name="theme" value="terminal" /><label for="mnu_theme_terminal"><span></span> Terminal</label>
+								<input type="radio" id="mnu_theme_tomorrow_night" name="theme" value="tomorrow_night" /><label for="mnu_theme_tomorrow_night"><span></span> Tomorrow Night</label>
+								<input type="radio" id="mnu_theme_tomorrow_night_blue" name="theme" value="tomorrow_night_blue" /><label for="mnu_theme_tomorrow_night_blue"><span></span> Tomorrow Night Blue</label>
+								<input type="radio" id="mnu_theme_tomorrow_night_bright" name="theme" value="tomorrow_night_bright" /><label for="mnu_theme_tomorrow_night_bright"><span></span> Tomorrow Night Bright</label>
+								<input type="radio" id="mnu_theme_tomorrow_night_eighties" name="theme" value="tomorrow_night_eighties" /><label for="mnu_theme_tomorrow_night_eighties"><span></span> Tomorrow Night 80s</label>
+								<input type="radio" id="mnu_theme_twilight" name="theme" value="twilight" /><label for="mnu_theme_twilight"><span></span> Twilight</label>
+								<input type="radio" id="mnu_theme_vibrant_ink" name="theme" value="vibrant_ink" /><label for="mnu_theme_vibrant_ink"><span></span> Vibrant Ink</label>
+							</div>
+						</div>
+					</div>
+				</div
+				><div class="drop drop_help_window">
+					<span><i class="fa fa-question"></i></span>
+					<div id="help_window">
+						<h2>Run PHP Code</h2>
+
+						<p>
+							<img src="img/website_duck.png" alt="" style="width: 40px; height: 40px;" /><br />
+							&copy; Website Duck LLC<br />
+						</p>
+
+						<a class="button" href="https://github.com/websiteduck/Run-PHP-Code">GitHub Repo</a><br />
+
+						<div style="margin-top: 10px;">
+							<b>Contributors</b>
+							<ul id="contributors"></ul>
+						</div>
+						
+					</div>
+				</div>
+					
+				<div id="button_container">
+					<button class="button" type="button" id="btn_reset"><i class="fa fa-eraser"></i> &nbsp; Clear</button>
+					<button class="button" type="button" id="btn_run" title="Run (Ctrl+Enter)">Run &nbsp; <i class="fa fa-play"></i></button>
+				</div>
+			</div>
+			
+			<div id="code_div"></div>
+			<input type="hidden" id="phprun_code" name="phprun_code" />
+		</form>
+		
+		<div id="result_div"><iframe id="result_frame" name="result_frame"></iframe></div>		
+		<div id="resize_bar"></div>
+		
+	</body>
+</html>
---
+++ php/js/run_php_code.js
@@ -0,0 +1,361 @@
+/**
+ * Run PHP Code
+ * 
+ * This script gives you the ability to quickly test snippets of PHP code locally.
+ *
+ * @copyright  Copyright 2011-2012, Website Duck LLC (http://www.websiteduck.com)
+ * @link       http://github.com/websiteduck/Run-PHP-Code Run PHP Code
+ * @license    MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+var rpc = {
+
+	loadSettings: function() {
+		rpc.settings = {};
+		if (localStorage.settings) rpc.settings = JSON.parse(localStorage.settings);
+		
+		//Default Settings
+		if (rpc.settings.runExternal === undefined) rpc.settings.runExternal = false;
+		if (rpc.settings.divideX === undefined || rpc.settings.divideX > $(window).width() - 10) rpc.settings.divideX = $(window).width() / 2;
+		if (rpc.settings.colorize === undefined) rpc.settings.colorize = true;
+		if (rpc.settings.theme === undefined) rpc.settings.theme = "twilight";
+		if (rpc.settings.preWrap === undefined) rpc.settings.preWrap = false;
+		if (rpc.settings.errorReporting === undefined) rpc.settings.errorReporting = 'fatal';			
+	},
+
+	saveSettings: function() {
+		localStorage.settings = JSON.stringify(rpc.settings);
+	},
+
+	menu: {
+
+		runExternal: function(runExternal, saveSettings) {
+	    if (runExternal) {
+				rpc.settings.runExternal = true;
+				$('#result_div').hide();
+				$('#resize_bar').hide();
+				$('#run_php_form').prop('target', 'result_external');
+			}
+			else {
+				rpc.settings.runExternal = false;
+				$('#result_div').show();
+				$('#resize_bar').show();
+				$('#run_php_form').prop('target', 'result_frame');
+			}
+			$(window).resize();
+			if (saveSettings) rpc.saveSettings();			
+		},
+
+		colorize: function(colorize, saveSettings) {
+			rpc.settings.colorize = colorize;
+			if (saveSettings) rpc.saveSettings();
+		},
+
+		theme: function(theme, saveSettings) {
+			rpc.settings.theme = theme;
+			rpc.editor.setTheme("ace/theme/" + rpc.settings.theme);
+			if (saveSettings) rpc.saveSettings();			
+		},
+
+		preWrap: function(preWrap, saveSettings) {
+			rpc.settings.preWrap = preWrap;
+			if (saveSettings) rpc.saveSettings();
+		},
+
+		errorReporting: function(errorReporting, saveSettings) {
+			rpc.settings.errorReporting = errorReporting;
+			if (saveSettings) rpc.saveSettings();
+		}
+
+	},
+
+	resizeWindow: function(width, height) {
+		if (rpc.settings.runExternal) {
+			$('#code_div').width(width);
+		}
+		else {
+			$('#code_div').width(rpc.settings.divideX - 2);
+			$('#resize_bar').css('left', rpc.settings.divideX - 2 + 'px');
+			$('#result_div').width((width - rpc.settings.divideX) - 2);
+		}
+		rpc.editor.resize();
+	},
+
+	resetEditor: function() {
+		rpc.editor.setValue("<?php\n\n");
+		rpc.editor.gotoLine(3);
+		rpc.editor.focus();
+		window.onbeforeunload = null;
+	},
+
+	setEditorContent: function(content) {
+		rpc.editor.setValue(content);
+		rpc.editor.gotoLine(1);
+		rpc.editor.focus();
+	},
+
+	runCode: function() {
+		$('input[name="phprun_action"]').val('run');
+		$('#run_php_form').submit();
+	}
+
+};
+
+window.onbeforeunload = null;
+
+$(function() {
+
+	rpc.loadSettings();
+
+	//Create ACE Editor
+	rpc.editor = ace.edit("code_div");
+	rpc.editor.getSession().setMode("ace/mode/php");
+	rpc.editor.setShowPrintMargin(false);
+	rpc.editor.commands.addCommand({
+		name: 'runCode',
+		bindKey: {win: 'Ctrl-Enter',  mac: 'Command-Enter'},
+		exec: function(editor) {
+			rpc.runCode();
+		}
+	});
+
+	//Set up menu elements
+	var $mnuExternalWindow = $('#mnu_external_window');
+	var $mnuColorize = $('#mnu_colorize');
+	var $mnuPreWrap = $('#mnu_pre_wrap');
+
+	if (rpc.settings.runExternal === true) $mnuExternalWindow.prop('checked', true);
+	if (rpc.settings.colorize === true) $mnuColorize.prop('checked', true);
+	if (rpc.settings.preWrap === true) $mnuPreWrap.prop('checked', true);
+	$('input[name="theme"][value="' + rpc.settings.theme + '"]').prop('checked', true);
+	$('input[name="error_reporting"][value="' + rpc.settings.errorReporting + '"]').prop('checked', true);
+
+	var $mnuTheme = $('input[name="theme"]'); 
+	var $mnuThemeSelected = $mnuTheme.filter(':checked');
+	var $mnuErrorReporting = $('input[name="error_reporting"]'); 
+	var $mnuErrorReportingSelected = $mnuErrorReporting.filter(':checked');
+
+	$mnuExternalWindow.change(function() { rpc.menu.runExternal(this.checked, true); });
+	rpc.menu.runExternal($mnuExternalWindow.get(0).checked, false);
+
+	$mnuColorize.change(function() { rpc.menu.colorize(this.checked, true); });
+	rpc.menu.colorize($mnuColorize.get(0).checked, false);
+
+	$mnuTheme.change(function() { rpc.menu.theme(this.value, true); });
+	rpc.menu.theme($mnuThemeSelected.get(0).value, false);
+
+	$mnuPreWrap.change(function() { rpc.menu.preWrap(this.checked, true); });
+	rpc.menu.preWrap($mnuPreWrap.get(0).checked, false);
+
+	$mnuErrorReporting.change(function() { rpc.menu.errorReporting(this.value, true); });
+	rpc.menu.errorReporting($mnuErrorReportingSelected.get(0).value, false);
+
+	rpc.editor.on('change', function() {
+		if (window.onbeforeunload == null) {
+			window.onbeforeunload = function() {
+				return "You have made changes in your editor.";
+			}
+		}
+	});
+		
+	$('#btn_reset').click(function() {
+		var answer = confirm("Are you sure you want to clear the editor?");
+		if (answer) { rpc.resetEditor(); rpc.runCode(); }
+	});
+	
+	$('#btn_run').click(function() { rpc.runCode(); });
+
+	rpc.editor.renderer.on('themeLoaded', function() {
+		var bgcolor = $('.ace_gutter').css('backgroundColor'); //base a lot of colors off this one
+		var color = $('#code_div').css('color');
+		$('#title_bar')
+			.css('background-color', bgcolor)
+			.css('color', color)
+			.css('box-shadow', '0 0 10px ' + shadeColor(bgcolor,-40) + ' inset');
+		$('.drop > div, .subdrop > div').css('box-shadow', '5px 5px 10px ' + shadeColor(bgcolor, -40));
+		$('.drop div, .subdrop, .drop label').css('backgroundColor', bgcolor);
+		$('.subdrop > div').css('border-top', '1px solid ' + shadeColor(bgcolor, -20));
+		$('.drop')
+			.css('backgroundColor', bgcolor)
+			.css('box-shadow', '0 10px 10px -10px ' + shadeColor(bgcolor, -40) + ' inset, 0px -10px 10px -10px ' + shadeColor(bgcolor, -40) + ' inset');
+		$('.button')
+			.css('backgroundColor', bgcolor)
+			.css('border-color', shadeColor(bgcolor,-20))
+			.css('box-shadow', '0px 1px 0px 0px ' + shadeColor(bgcolor,20) + ' inset')
+			.css('color', color);
+		$('#resize_bar').css('backgroundColor', shadeColor(bgcolor,-20));
+		if (rpc.settings.colorize) {
+			$('#result_frame').css('backgroundColor', shadeColor(bgcolor,-2));
+			$('#result_frame').contents().find('html')
+				.css('backgroundColor', shadeColor(bgcolor,-2))
+				.css('color', color);
+		}
+		$('.hovered').each(function() {
+			$(this).css('backgroundColor', shadeColor($(this).css('backgroundColor'),5));
+		});
+	});
+	$('#result_frame').load(function() { 
+		if (rpc.settings.colorize) {
+			var bgcolor = $('.ace_gutter').css('backgroundColor'); //base a lot of colors off this one
+			var color = $('#code_div').css('color');
+
+			$('#result_frame').contents().find('html')
+				.css('backgroundColor', shadeColor(bgcolor,-2))
+				.css('color', color);
+			}
+	});
+	
+	$(window).resize(function() { rpc.resizeWindow($(this).width(), $(this).height()); }).resize();
+	
+	var resizing = false;
+	$(document).mousemove(function(e) {
+		if (resizing === true) {
+			var x = e.pageX;
+			if (x < 100) x = 100;
+			rpc.settings.divideX = x;
+			$(window).resize();
+		}
+	});
+	$('#resize_bar').mousedown(function(e) {
+		if (rpc.settings.runExternal === false) {
+			resizing = true; 
+			$('#result_frame').css('pointer-events', 'none');
+			e.preventDefault();
+		}
+	}).mouseup(function(e) {
+		if (resizing) {
+			resizing = false;
+			$('#result_frame').css('pointer-events', 'auto');
+			rpc.saveSettings();
+		}
+	});
+		
+	var hide_menu_timeout = [];
+	$('.drop').hover(function() {
+		clearTimeout(hide_menu_timeout[$(this).uniqueId().attr('id')]);
+		$('.drop div').css('z-index', '9997');
+		$('> div', this).css('z-index', '9998').stop(true,true,true).slideDown(100);
+	}, function() {
+		var self = this;
+		hide_menu_timeout[$(this).uniqueId().attr('id')] = setTimeout(function() { $('> div', self).stop(true,true,true).slideUp(100); }, 300);
+	});
+	
+	$('.subdrop').hover(function() {
+		clearTimeout(hide_menu_timeout[$(this).uniqueId().attr('id')]);
+		$('.subdrop').css('z-index', '9990');
+		$(this).css('z-index', '9991');
+		//Doing this instead of hide/show because of a bug in chrome that leaves part of the menu on the screen
+		$('> div', this).css('left', '200px');
+	}, function() {
+		var self = this;
+		hide_menu_timeout[$(this).uniqueId().attr('id')] = setTimeout(function() { $('> div', self).css('left', '-9999px'); }, 300);
+	});
+
+	$('.drop, .subdrop, .drop label, .button').hover(function() {
+		$(this).addClass('hovered');
+		$(this).css('backgroundColor', shadeColor($(this).css('backgroundColor'), 5));
+	}, function() {
+		$(this).removeClass('hovered');
+		$(this).css('backgroundColor', shadeColor($(this).css('backgroundColor'), -5));
+	});
+
+	function get_id_from_url(url) {
+		var url_array = url.split('/');
+		return url_array[url_array.length-1];
+	}
+	
+	$('#btn_import').click(function() {
+		var code_url = prompt('Always make sure imported code is safe before running!!!\n\nSupported services: gist.GitHub.com, PasteBin.com, Pastie.org\n\nEnter URL:');
+		if (code_url === null || code_url === '') return;
+		code_id = get_id_from_url(code_url);
+		rpc.editor.setValue('Loading code...');
+		
+		if (code_url.toLowerCase().indexOf('github.com') !== -1) {
+			$.get('proxy.php', {url: 'https://api.github.com/gists/' + code_id}, function(data) {
+				if (data.charAt(0) === '{') {
+					data = $.parseJSON(data);
+					var content = '';
+					for (var i in data.files) content += data.files[i].content + '\n';
+					rpc.setEditorContent(content);
+				}
+				else {
+					rpc.setEditorContent(data);
+				}
+			}, 'text');
+		}
+		else if (code_url.toLowerCase().indexOf('pastebin.com') !== -1) {
+			$.get('proxy.php', {url: 'http://pastebin.com/raw.php?i=' + code_id}, function(data) {
+				rpc.setEditorContent(data);
+			}, 'text');
+		}
+		else if (code_url.toLowerCase().indexOf('pastie.org') !== -1) {
+			$.get('proxy.php', {url: 'http://pastie.org/pastes/' + code_id + '/download'}, function(data) {
+				rpc.setEditorContent(data);
+			}, 'text');
+		}
+	});
+	
+	$('#btn_download').click(function() {
+		var filename = prompt('Filename:');
+		if (filename === null || filename === '') return;
+		$('input[name="phprun_action"]').val('download');
+		$('input[name="phprun_filename"]').val(filename);
+		$('#run_php_form').submit();
+	});
+	
+	$('#title_bar').click(function() { setTimeout(function() {rpc.editor.focus();}, 50); });
+	
+	rpc.resetEditor();
+	if (rpc.settings.runExternal === false) rpc.runCode(); //If running externally we don't want a new tab to open right away
+
+	var contributors_loaded = false;
+	$('.drop_help_window').hover(function() {
+		if (contributors_loaded === false) {
+			contributors_loaded = true;
+			$.get('https://api.github.com/repos/websiteduck/Run-PHP-Code/contributors', function(data) {
+				for (var i = 0; i < data.length; i++) {
+					$('#contributors').append(
+						'<li>' + 
+							'<label><a href="' + data[i].html_url + '">' +
+								'<img src="' + data[i].avatar_url + '&s=24" /> ' + data[i].login + 
+							'</a></label>' + 
+						'</li>'
+					);
+				}
+			}, 'json');
+		}
+	});
+	
+});
+
+function run_php_form_submit() {
+	$('#phprun_code').val(rpc.editor.getValue());
+}	
+
+//http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color
+function shadeColor(color, percent) {   
+	color = color.replace(/#/,'');
+	var num = parseInt(color,16),
+	amt = Math.round(2.55 * percent),
+	R = (num >> 16) + amt,
+	B = (num >> 8 & 0x00FF) + amt,
+	G = (num & 0x0000FF) + amt;
+	return '#' + (0x1000000 + (R<255?R<1?0:R:255)*0x10000 + (B<255?B<1?0:B:255)*0x100 + (G<255?G<1?0:G:255)).toString(16).slice(1);
+}
+
+$.cssHooks.backgroundColor = {
+    get: function(elem) {
+        if (elem.currentStyle)
+            var bg = elem.currentStyle["backgroundColor"];
+        else if (window.getComputedStyle)
+            var bg = document.defaultView.getComputedStyle(elem, null).getPropertyValue("background-color");
+
+        if (bg.search("rgb") == -1)
+            return bg;
+        else {
+            bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+            function hex(x) { return ("0" + parseInt(x).toString(16)).slice(-2); }
+            return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
+        }
+    }
+}
